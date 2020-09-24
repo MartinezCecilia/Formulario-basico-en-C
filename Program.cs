a2a14eb208e829e48d9b6dc99ec37bb0a8bfc4b2
MartinezCecilia <71801400+MartinezCecilia@users.noreply.github.com>
2020-09-23 22:24:07 -0500

    Formulario basico en C

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Formulario_basico_en_c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String Nombre;
+            String ApellidoPaterno;
+            String ApellidoMaterno;
+            String Ocupacion;
+            String Correo;
+            String Mes;
+            int Edad;
+
+            Console.WriteLine("REGISTRO EN EL FORMULARIO");
+        }
+    }
+}
